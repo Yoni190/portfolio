3431d05fcb7558372b3c93b9e0e5c60dcc0384d3
Yoni190 <yfisuhu@gmail.com>
2026-01-17 15:03:09 +0300

    Create about section

---
+++ src/components/About.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import HtmlImage from '../assets/html.png'
+import CssImage from '../assets/css.png'
+import JsImage from '../assets/js.png'
+import ReactImage from '../assets/react.svg'
+import LaravelImage from '../assets/laravel.svg'
+import MysqlImage from '../assets/mysql.png'
+import GitImage from '../assets/git.png'
+
+const About = () => {
+    const skills = [
+        {name: 'HTML', img: HtmlImage},
+        {name: 'CSS', img: CssImage},
+        {name: 'JS', img: JsImage},
+        {name: 'React', img: ReactImage},
+        {name: 'Laravel', img: LaravelImage},
+        {name: 'MySQL', img: MysqlImage},
+        {name: 'React Native', img: ReactImage},
+        {name: 'Git', img: GitImage},
+    ]
+  return (
+    <div className='mt-4'>
+        <h2 className='text-2xl text-center'>About</h2>
+
+        <div className='flex justify-center gap-3'>
+            {/* Paragraph Section */}
+            <div className='w-1/2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sunt debitis ratione voluptates, odio iusto voluptatem dicta doloribus veritatis ab fuga non nobis enim recusandae, ex eius ad, quis facere! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis hic quae tempora accusantium nostrum veniam nam totam blanditiis ullam voluptatem temporibus tempore nesciunt a eos veritatis enim soluta, ipsa nemo! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo esse quia atque aspernatur, nesciunt quaerat dignissimos? Libero sequi dolores minus ut architecto voluptate incidunt nam culpa, itaque ad rem eligendi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione laudantium id quas fugit error, excepturi et ipsum iure nam magni, obcaecati similique at commodi iste, ipsa mollitia ipsam. Eius, dolor.
+            </div>
+            {/* Skills section */}
+            <div className='flex gap-5  grid grid-cols-4'>
+                {skills.map((skill) => (
+                    <div key={skill.name} className='border rounded p-4 flex flex-col items-center hover:shadow-xl transition duration-500'>
+                        <img src={skill.img} alt={skill.name} width="50"/>
+                        <p className='text-center'>{skill.name}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
