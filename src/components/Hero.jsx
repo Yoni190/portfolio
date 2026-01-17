c145ed98721cc923ee0ccccaa9b1257fc382d087
Yoni190 <yfisuhu@gmail.com>
2026-01-17 20:13:48 +0300

    Add hover effect on view my work button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='h-screen flex flex-col justify-center items-center bg-black'>
         <h1 className='text-6xl text-white text-center'>Hello, I'm <span className='text-blue-400'>Yonatan</span>. <br /> I'm a Full Stack Developer.</h1>
-        <button className='border-2 border-blue-400 mt-2 px-10 py-2 text-blue-400 transition duration-500 hover:shadow-xl/30 hover:cursor-pointer'>View My Work</button>
+        <button className='mt-5 border-2 border-blue-400 mt-2 px-10 py-2 text-blue-400 transition duration-500 hover:scale-110 hover:bg-blue-400 hover:text-black hover:cursor-pointer'>View My Work</button>
     </div>
   )
 }
