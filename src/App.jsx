3f8ed6067608e4fae85de6503f6eed3bd8bfb669
Yoni190 <yfisuhu@gmail.com>
2026-01-17 16:27:55 +0300

    Add pixel snow animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import Contact from "./components/Contact"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import PixelSnow from "./components/PixelSnow"
 import Projects from "./components/Projects"
+
 
 function App() {
 
   return (
-    <>
+    <div>
+      <PixelSnow
+        pixelResolution={500}
+      />
       <Hero />
       <Navbar />
       <About />
       <Projects />
       <Contact />
       <Footer />
-    </>
+    </div>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const Hero = () => {
   return (
-    <div className='h-screen flex flex-col justify-center items-center'>
-        <h1 className='text-6xl text-center'>Hello, I'm <span className='text-blue-400'>Yonatan</span>. <br /> I'm a Full Stack Developer.</h1>
+    <div className='h-screen flex flex-col justify-center items-center bg-black'>
+        <h1 className='text-6xl text-white text-center'>Hello, I'm <span className='text-blue-400'>Yonatan</span>. <br /> I'm a Full Stack Developer.</h1>
         <button className='border-2 border-blue-400 mt-2 px-10 py-2 text-blue-400 transition duration-500 hover:shadow-xl/30 hover:cursor-pointer'>View My Work</button>
     </div>
   )
