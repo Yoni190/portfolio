08ec764c07ab74d892b86973a6b3107dc416b630
Yoni190 <yfisuhu@gmail.com>
2026-01-17 21:05:25 +0300

    Add hover effect to navbar elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = ({ onHomeClick, onAboutClick, onProjectsClick, onContactClick }) 
   return (
     <div className='border py-3 bg-gray-900'>
         <ul className='flex gap-5 justify-center text-white text-xl'>
-            <li className='hover:cursor-pointer hover:text-blue-400'><button onClick={onHomeClick}>Home</button></li>
-            <li className='hover:cursor-pointer hover:text-blue-400'><button onClick={onAboutClick}>About</button></li>
-            <li className='hover:cursor-pointer hover:text-blue-400'><button onClick={onProjectsClick}>Projects</button></li>
-            <li className='hover:cursor-pointer hover:text-blue-400'><button onClick={onContactClick}>Contact</button></li>
+            <li><button className='hover:cursor-pointer hover:text-blue-400' onClick={onHomeClick}>Home</button></li>
+            <li><button className='hover:cursor-pointer hover:text-blue-400' onClick={onAboutClick}>About</button></li>
+            <li><button className='hover:cursor-pointer hover:text-blue-400' onClick={onProjectsClick}>Projects</button></li>
+            <li><button className='hover:cursor-pointer hover:text-blue-400' onClick={onContactClick}>Contact</button></li>
         </ul>
     </div>
   )
