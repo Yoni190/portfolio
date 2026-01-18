c12a9aefcceb09034ae6f5d2bbaab98a1c977141
Yoni190 <yfisuhu@gmail.com>
2026-01-18 17:00:04 +0300

    Hide the pixelsnow animation on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,28 @@ function App() {
       ref.current.scrollIntoView({ behavior: 'smooth' });
     };
 
+    const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
+
   return (
     <div>
-      <PixelSnow 
-        color="#ffffff"
-        flakeSize={0.01}
-        minFlakeSize={1.25}
-        pixelResolution={190}
-        speed={1.25}
-        density={0.25}
-        direction={125}
-        brightness={1}
-        depthFade={8}
-        farPlane={20}
-        gamma={0.4545}
-        variant="square"
-        className='z-0'
-      />
+      {!isMobile && (
+        <PixelSnow 
+          color="#ffffff"
+          flakeSize={0.01}
+          minFlakeSize={1.25}
+          pixelResolution={190}
+          speed={1.25}
+          density={0.25}
+          direction={125}
+          brightness={1}
+          depthFade={8}
+          farPlane={20}
+          gamma={0.4545}
+          variant="square"
+          className='z-0'
+        />
+      )}
+      
 
 
       <div ref={homeRef}>
