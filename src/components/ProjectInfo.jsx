4bbd71a2bc9db6fb40b77d281ad0328d04c04ac7
Yoni190 <yfisuhu@gmail.com>
2026-01-18 17:06:29 +0300

    Add lazy loading to images of the projects

--- src/components/ProjectInfo.jsx
+++ src/components/ProjectInfo.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 const ProjectInfo = ({ title, desc, img, live="", git="", index}) => {
   return (
-    <div className={`flex ${index % 2 ? 'flex-row-reverse' : 'flex-row'} justify-center mt-5`}>
+    <div className={`flex flex-col lg:flex-row ${index % 2 ? 'lg:flex-row-reverse' : 'lg:flex-row'} justify-center mt-5 px-4`}>
         {/* Project Image */}
-        <div className='relative'>
+        <div className='relative w-full lg:w-auto'>
             <a href={live ? live : git} target='_blank' rel='noopener noreferrer'>
-              <img src={img} alt={title} width={1000} className='hover:scale-80 transition duration-500'/>
+              <img 
+                src={img} 
+                alt={title} 
+                loading="lazy"
+                className='w-full max-w-full h-auto object-contain hover:scale-95 transition duration-500'
+                style={{ maxWidth: '600px' }}
+              />
             </a>
         </div>
 
         {/* Project Info */}
-        <div className='p-5 w-1/4 flex flex-col justify-center'>
+        <div className='p-5 w-full lg:w-1/4 flex flex-col justify-center'>
             <h1 className='font-semibold text-4xl'>{title}</h1>
             <p className='mt-2 text-lg'>{desc}</p>
 
