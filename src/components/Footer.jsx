f855b958b44f7813ecd53d0532e941bee8cfaa2a
Yoni190 <yfisuhu@gmail.com>
2026-01-18 17:07:18 +0300

    Add lazy loading to footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <div className='border relative px-7 py-10 flex flex-col gap-2 items-center bg-gray-900'>
         <div className='flex gap-4'>
-            <a href="https://github.com/Yoni190" target='_blank'><img src={GithubImage} alt="Github" width={40} className='hover:scale-110 transition duration-300' /></a>
-            <a href="mailto:yonatanadhanom00@gmail.com" target='_blank'><img src={EmailImage} alt="Email" width={40} className='hover:scale-110 transition duration-300' /></a>
+            <a href="https://github.com/Yoni190" target='_blank'><img src={GithubImage} alt="Github" width={40} height={40} loading="lazy" className='hover:scale-110 transition duration-300' /></a>
+            <a href="mailto:yonatanadhanom00@gmail.com" target='_blank'><img src={EmailImage} alt="Email" width={40} height={40} loading="lazy" className='hover:scale-110 transition duration-300' /></a>
         </div>
         <p className='text-white text-sm'>Yonatan Adhanom © {new Date().getFullYear()}</p>
     </div>
