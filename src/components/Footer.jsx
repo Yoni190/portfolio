a3fda85198b56d6d77682f5ac7df2ae04a634350
Yoni190 <yfisuhu@gmail.com>
2026-08-20 00:57:33 +0300

    chore: change contact email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='border relative px-7 py-10 flex flex-col gap-2 items-center bg-gray-900'>
         <div className='flex gap-4'>
             <a href="https://github.com/Yoni190" target='_blank' rel="noopener noreferrer"><img src={GithubImage} alt="Github" width={40} height={40} loading="lazy" className='hover:scale-110 transition duration-300' /></a>
-            <a href="mailto:yonatanadhanom00@gmail.com" target='_blank' rel="noopener noreferrer"><img src={EmailImage} alt="Email" width={40} height={40} loading="lazy" className='hover:scale-110 transition duration-300' /></a>
+            <a href="mailto:yfisuhu@gmail.com" target='_blank' rel="noopener noreferrer"><img src={EmailImage} alt="Email" width={40} height={40} loading="lazy" className='hover:scale-110 transition duration-300' /></a>
         </div>
         <p className='text-white text-sm'>Yonatan Adhanom © {new Date().getFullYear()}</p>
     </div>
